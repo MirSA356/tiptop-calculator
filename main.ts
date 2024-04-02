e07f88d13372a1c9718f3a73cce238558cf014bb
Your Name <you@example.com>
2024-04-02 16:38:12 +0500

    little changes in 8th and 10th row and add last ending line

--- main.ts
+++ main.ts
@@ -5,9 +5,9 @@
 import inquirer from "inquirer";
 
 const answer = await inquirer.prompt([
-     {message: "Enter first number", type: "number",
+     {message: "Enter your first number", type: "number",
 name: "fisrtNumber" }, 
-{message: "Enter second number", type: "number", name: "secondNumber" },
+{message: "Enter your second number", type: "number", name: "secondNumber" },
 
 {message: "select one of the operators to perform operation",
  type: "list",
@@ -34,7 +34,7 @@ else if(answer.operator === "Division") {
 else{
     console.log("please select a valid operator")}
 
-
+console.log(the end);
 
 // move to desired folder 
 // run npm init 
