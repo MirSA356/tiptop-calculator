22085cc543e2e5022f10fcc7f902919d4370d27e
Your Name <you@example.com>
2024-04-02 17:38:28 +0500

    little changes in 8th and 10th row and add last ending line 2

--- main.ts
+++ main.ts
@@ -5,38 +5,35 @@
 import inquirer from "inquirer";
 
 const answer = await inquirer.prompt([
-     {message: "Enter your first number", type: "number",
-name: "fisrtNumber" }, 
-{message: "Enter your second number", type: "number", name: "secondNumber" },
+  { message: "Enter your first number", type: "number", name: "fisrtNumber" },
+  { message: "Enter your second number", type: "number", name: "secondNumber" },
 
-{message: "select one of the operators to perform operation",
- type: "list",
-  name: "operator",
-   choices: ["Addition","Subtraction","Multiplication","Division"],
-},
+  {
+    message: "select one of the operators to perform operation",
+    type: "list",
+    name: "operator",
+    choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+  },
 ]);
 
 // console.log(answer);
 
-// conditonal statement 
- if (answer.operator === "Addition") {
-    console.log(answer. fisrtNumber + answer. secondNumber);
+// conditonal statement
+if (answer.operator === "Addition") {
+  console.log(answer.fisrtNumber + answer.secondNumber);
+} else if (answer.operator === "Subtration") {
+  console.log(answer.fisrtNumber - answer.secondNumber);
+} else if (answer.operator === "Multiplication") {
+  console.log(answer.fisrtNumber * answer.secondNumber);
+} else if (answer.operator === "Division") {
+  console.log(answer.fisrtNumber / answer.secondNumber);
+} else {
+  console.log("please select a valid operator");
 }
-else if (answer.operator === "Subtration") {
-    console.log(answer. fisrtNumber - answer. secondNumber);
-}
-else if (answer.operator === "Multiplication") {
-    console.log(answer. fisrtNumber * answer. secondNumber);
-}
-else if(answer.operator === "Division") {
-    console.log(answer. fisrtNumber / answer. secondNumber);
-}
-else{
-    console.log("please select a valid operator")}
 
-console.log(the end);
+console.log("the end");
 
-// move to desired folder 
-// run npm init 
+// move to desired folder
+// run npm init
 // run tsc --init
 // install inquirer, @types/ inquirer code
